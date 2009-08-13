0e5ccedad64c1309159e3227e1635e26d507166b
Mike <mkrieger@meebos-macbook.local>
2009-08-13 01:47:44 -0700

    incorporating changes for bash script

--- tools/prowl-gv.sh
+++ tools/prowl-gv.sh
@@ -2,6 +2,8 @@
 #
 # Script to start/stop the Prowl Google Voice daemon
 #
+# v1.1 - Added status option
+#      - Fixed some typos, moved the $# check to main
 # v1.0 - Tested and working - released.
 #
 # Copyright (C) 2008 James Bair <james.d.bair@gmail.com>
@@ -20,11 +22,12 @@
 # along with this program; if not, write to the Free Software Foundation,
 # Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 
-# Where to find the main script
+# Where to find the main script we're going to run in the background.
 file=prowlgooglevoice.py
+# Name of this script.
 script=$(basename $0)
 
-# Since we check this status a lot, we'll just re-create the variable as needd.
+# Since we check this status a lot, we'll just re-create the variable as needed.
 findOurPID() {
 	ourPID=$(ps ax | while read pid a b c d script; do [ "$script" == "$file" ] && echo $pid; done)
 	export ourPID
@@ -32,14 +35,8 @@ findOurPID() {
 
 # General usage function
 usage() {
-	echo "USAGE: $script - <start|stop|restart>"
+	echo "USAGE: $script - <start|stop|restart|status>"
 }
-
-# Need an argument, no more, no less.
-if [ $# -ne 1 ]; then
-	usage
-	exit 1
-fi
 
 # Start
 startFile() {
@@ -47,17 +44,17 @@ startFile() {
 	# Find our PID
 	findOurPID
 
-	# Make sure we're not already running.
+	# Make sure we're not already running
 	if [ -n "$ourPID" ]; then
 		echo "$file is already running."
 	else
 	
-		# If it's a file and non-zero, then call it with Python.
+		# If it's a file and non-zero, then call it with Python
 		if [ -f $file -a -s $file ]; then
 			python $file > /dev/null 2>&1 &
 			echo "$file is now running in the background."
 		else
-			echo "$file is either missing or not executable. Exiting."
+			echo "$file is missing. Exiting."
 			exit 1
 		fi
 	fi
@@ -68,7 +65,7 @@ stopFile() {
 	# Find our PID
 	findOurPID
 
-	# Make sure we're actually running.
+	# Make sure we're actually running
 	if [ -z "$ourPID" ]; then
 		echo "$file is not running."
 	else
@@ -88,14 +85,35 @@ stopFile() {
 	fi
 }
 
+fileStatus() {
+
+	# Find our PID
+	findOurPID
+
+	# If it's not empty, it's running
+	if [ -n "$ourPID" ]; then
+		echo "$file is currently running in the background."
+	# If it's empty, it's not running
+	else
+		echo "$file is not running."
+	fi
+
+}
+
 ############
 ### MAIN ###
 ############
 
-# Make sure all requird libraries and configs exist
-mustHaves="credentials googlevoicenotify.py prowlgooglevoice.py prowlkey prowlpy.py"
+# We need one argument, no more, no less
+if [ $# -ne 1 ]; then
+	usage
+	exit 1
+fi
+
+# Make sure all required libraries and configs exist
+mustHaves="$file credentials googlevoicenotify.py prowlkey prowlpy.py"
 for i in $mustHaves; do
-	# Verify it's non-zero
+	# Verify they are all non-zero
 	if [ ! -s $i ]; then
 		echo "ERROR: Missing required file: $i - Exiting."
 		exit 1
@@ -118,6 +136,11 @@ case $1 in
 		stopFile && startFile
 		exit 0
 		;;
+
+	status)
+		fileStatus
+		exit 0
+		;;	
 
 	*)
 		usage
